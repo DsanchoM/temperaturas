9a4784d99986d26bf588617a08f40a95ad40a434
David Sancho Manzano <dsanchom@campusdigitalfp.com>
2026-09-10 12:08:23 +0200

    primer diccionario

---
+++ diccionarios.py
@@ -0,0 +1,19 @@
+dias = [
+    {
+        "ciudad" : "Zaragoza",
+        "temperatura" : 40,
+        "humedad" : 45
+    },
+    {
+        "ciudad" : "Madrid",
+        "temperatura" : 35,
+        "humedad" : 50
+    },
+    {
+        "ciudad" : "Barcelona",
+        "temperatura" : 30,
+        "humedad" : 65
+    }
+]
+
+print(dias[0]["ciudad"])
