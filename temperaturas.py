b0734d88e98fbe7fa8d5ec13abe126067e0a2f76
David Sancho Manzano <dsanchom@campusdigitalfp.com>
2026-09-10 10:05:32 +0200

    Version 5: Funciones

--- temperaturas.py
+++ temperaturas.py
@@ -1,29 +1,24 @@
-temperaturas = [18.5, 21.2, 19.8, 23.1, 25.4]
 
-print ("Las temperaturas registradas han sido las siguientes:", temperaturas)
+# Generación de funciones
 
-#----------------------------------------------
-media = sum(temperaturas) / len(temperaturas)
+# Calculo de la media
+def calcular_media(temperaturas):
+    return sum(temperaturas)/len(temperaturas)
 
-print("Temperatura media:", media)
+# Obtención de la máxima
+def obtener_maxima(temperaturas):
+    return max(temperaturas)
 
-# Pregunta 1 : ¿Cuál ha sido la temperatura máxima?
-maxima_temperatura = max(temperaturas)
+# Obtención de la minima
+def obtener_minima(temperaturas):
+    return min(temperaturas)
 
-print("La temperatura máxima registrada ha sido de ", maxima_temperatura) 
+temperaturas=[18.5, 21.2, 19.8, 23.1, 25.4]
 
-# Pregunta 2 : ¿Cuál ha sido la temperatura mínima?
-minima_temperatura = min(temperaturas)
+media=calcular_media(temperaturas)
+maxima=obtener_maxima(temperaturas)
+minima=obtener_minima(temperaturas)
 
-print("La temperatura mínima registrada ha sido de ", minima_temperatura)
-
-# Pregunta 3 : ¿Qué temperaturas son superiores a 22 ºC?
-for temperatura in temperaturas:
-    if temperatura > 22:
-        print("Las temperaturas superiores a 22º han sido las siguientes:")
-        print(temperatura)
-
-# Pregunta 4: ¿Ha hecho algún día más de 25 ºC?
-for temperatura in temperaturas:
-    if temperatura > 25:
-        print("Sí, ha habido una temperatura superior a 25 ºC")
+print("Media:",media)
+print("Máxima:",maxima)
+print("Mínima",minima)
