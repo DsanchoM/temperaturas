7d1271918630fd3aebc7c88eccf6464fb4f80737
David Sancho Manzano <dsanchom@campusdigitalfp.com>
2026-09-10 10:39:07 +0200

    Version 5: Solo Funciones

--- temperaturas.py
+++ temperaturas.py
@@ -12,13 +12,3 @@ def obtener_maxima(temperaturas):
 # Obtención de la minima
 def obtener_minima(temperaturas):
     return min(temperaturas)
-
-temperaturas=[18.5, 21.2, 19.8, 23.1, 25.4]
-
-media=calcular_media(temperaturas)
-maxima=obtener_maxima(temperaturas)
-minima=obtener_minima(temperaturas)
-
-print("Media:",media)
-print("Máxima:",maxima)
-print("Mínima",minima)
